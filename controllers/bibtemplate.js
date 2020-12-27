fb004b67b2d7961574ac19bef6f542f4c8c21684
pongpatsi56 <ps43638@gmail.com>
2020-12-27 14:57:08 +0700

    add template selecter

--- controllers/bibtemplate.js
+++ controllers/bibtemplate.js
@@ -1,10 +1,17 @@
 const { template, temp_databib } = require('../models');
-const { Op } = require('sequelize');
+// const { Op } = require('sequelize');
 const helper = require('../helper/stringHelper')
+
+exports.list_select_template = (req,res) =>{
+    try {
+        template.findAll({}).then(outp => res.send(outp));
+    } catch (e) {
+        throw e;
+    }
+};
 
 exports.list_templatebyId = async (req, res) => {
     try {
-        const Keyword = req.params.keyword != null ? req.params.keyword : '';
         await template.findAll({
             attributes: ['template_ID', 'Name', 'Type', 'Description'],
             include: [
@@ -14,9 +21,7 @@ exports.list_templatebyId = async (req, res) => {
                 }
             ],
             where: {
-                Name: {
-                    [Op.substring]: Keyword
-                }
+                template_ID: req.params.templateId
             }
         }).then((db) => {
             if (db) {
@@ -26,7 +31,6 @@ exports.list_templatebyId = async (req, res) => {
             }
             res.send(db);
         });
-
     } catch (e) {
         console.log(e);
         throw e;
--- routes/BibTemplate.js
+++ routes/BibTemplate.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const BibTemplateControllers = require('../controllers/bibtemplate');
 
 /* get field indc1,2 subfield show for add book */
-router.get("/listtemplatebib/:keyword", BibTemplateControllers.list_templatebyId);
+router.get("/listTempSelect", BibTemplateControllers.list_select_template);
+
+/* get field indc1,2 subfield show for add book */
+router.get("/listtemplatebib/:templateId", BibTemplateControllers.list_templatebyId);
 
 
 module.exports = router;
